521d951fb1e48e8fece3c896be8bd0606ae6c8c1
VedangSardessai <vms112000@gmail.com>
2023-08-23 09:56:37 +0530

    code: integration of all components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import News from "./components/news/News";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./App.css";
+import styled from "styled-components";
+import { useState } from "react";
+import "./firebase";
 function App() {
+  const [gridActive, setGridActive] = useState(false);
+  const [listening, setlistening] = useState(true);
+  const [blurEffect, setBlurEffect] = useState(false); // Add blurEffect state
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Sidebar
+         
+          gridActive={gridActive}
+          setGridActive={setGridActive}
+          blurEffect={blurEffect} // Pass blurEffect to Sidebar
+          setBlurEffect={setBlurEffect} // Pass setBlurEffect to Sidebar
+        />
+        <News gridActive={gridActive} blurEffect={blurEffect} />{" "}
+        {/* Pass blurEffect to News */}
+      </Container>
     </div>
   );
 }
 
+const Container = styled.div`
+  display: flex;
+`;
 export default App;
